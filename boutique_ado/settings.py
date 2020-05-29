92c73fcd15cf6ed2f8f7852ed078d7187d01ab81
StefanoS <iiostefanos@gmail.com>
2020-05-29 09:03:45 +0000

    added main-nav, mobile header, products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -61,9 +62,9 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates','allauth'),
-
+            os.path.join(BASE_DIR, 'templates','allauth'), 
         ],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
